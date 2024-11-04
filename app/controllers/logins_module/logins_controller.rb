83d62b92c6bbd63bf146691940725753c20f4057
Abhishek-Singh-98 <avi04427@gmail.com>
2024-11-04 10:54:09 +0530

    login code is optimized

--- app/controllers/logins_module/logins_controller.rb
+++ app/controllers/logins_module/logins_controller.rb
@@ -1,23 +1,25 @@
 class LoginsModule::LoginsController < LoginsModule::ApplicationController
+  before_action :find_user
 
   def login_user
+    if @user&.authenticate(params[:camera_man][:password])
+      @token = JwtAuthentication.encode(id: @user.id)
+      render json: {email: @user.email, token: @token}
+    else
+      render json: {error: 'Invalid Password or Email'}
+    end
+  end
+
+  private
+
+  def find_user
+    return render json: {error: "user_role missing"} if params[:user_role].empty?
+
     case params[:user_role]
     when 'camman'
-      @camera_man = UsersModule::CameraMan.find_by(email: params[:camera_man][:email].downcase)
-      if @camera_man&.authenticate(params[:camera_man][:password])
-        @token = JwtAuthentication.encode(id: @camera_man.id)
-        render json: {email: @camera_man.email, token: @token}
-      else
-        render json: {error: 'Invalid Password or Email'}
-      end
+      @user = UsersModule::CameraMan.find_by(email: params[:camera_man][:email].downcase)
     when 'client'
-      @client = UsersModule::Client.find_by(email: params[:client][:email].downcase)
-      if @client&.authenticate(params[:client][:password])
-        @token = JwtAuthentication.encode(id: @client.id)
-        render json: {email: @client.email, token: @token}
-      else
-        render json: {error: 'Invalid Password or Email'}
-      end
+      @user = UsersModule::Client.find_by(email: params[:client][:email].downcase)
     end
   end
 end
